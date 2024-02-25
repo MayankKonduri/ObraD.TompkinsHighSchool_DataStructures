826857650dfe1b5cb0b33fe8d104e5a0eb474b55
MayankKonduri <94502502+MayankKonduri@users.noreply.github.com>
2024-02-24 20:57:41 -0600

    Add files via upload

---
+++ Heaps-PriorityQueues/L2-PriorityQueues/MyPriorityQueue.java
@@ -0,0 +1,45 @@
+public class MyPriorityQueue<E extends Comparable<E>> implements PriorityQueueInterface<E>{
+    private MinHeap<E> mH;
+    public MyPriorityQueue(){
+        mH = new MinHeap<>();
+    }
+    public boolean offer(E o)
+    {
+        return mH.insert(o);
+    }
+    public E poll()
+    {
+        return mH.remove();
+    }
+    public int size()
+    {
+        return mH.size();
+    }
+    public boolean isEmpty()
+    {
+        return mH.isEmpty();
+    }
+    public void clear()
+    {
+        mH.clear();
+    }
+    public E get(int x)
+    {
+        return mH.get(x);
+    }
+    public E element()
+    {
+        if(mH.isEmpty())
+        {
+            return null;
+        }
+        else
+        {
+            return mH.get(0);
+        }
+    }
+    public String toString()
+    {
+        return mH.toString();
+    }
+}
