87998387f74cbf280829d897bad6337f6814b994
MayankKonduri <94502502+MayankKonduri@users.noreply.github.com>
2024-02-24 21:09:18 -0600

    Add files via upload

---
+++ Sorts-Searches/CH2-BinearSearch/Sort-2.java
@@ -0,0 +1,20 @@
+public class Sort{
+    public static void insertionSort(int[] list)
+    {
+        for(int i=1;i<list.length;i++)
+        {
+            int temp = list[i];
+            int j = i;
+            while(j>0 && list[j-1]>temp)
+            {
+                list[j] = list[j-1];
+                j=j-1;
+                list[j] = temp;
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
